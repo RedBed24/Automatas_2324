82978b46065ff36870152b7f9c998f0a4a8b96f7
RedBed24 <69651116+RedBed24@users.noreply.github.com>
2024-02-14 09:13:42 +0100

    Add styling of class App

--- subjava-compiler/src/main/java/automatas/c2324/e01/App.java
+++ subjava-compiler/src/main/java/automatas/c2324/e01/App.java
@@ -4,8 +4,19 @@ package automatas.c2324.e01;
  * Hello world!
  *
  */
-public class App {
-    public static void main(String[] args) {
+public final class App {
+    /**
+     * Default private constructor.
+     */
+    private App() {
+    };
+
+    /**
+     * Main entry point of the program.
+     *
+     * @param args argumentos obtenidos de la línea de comandos
+     */
+    public static void main(final String[] args) {
         System.out.println("Hello World!");
     }
 }
